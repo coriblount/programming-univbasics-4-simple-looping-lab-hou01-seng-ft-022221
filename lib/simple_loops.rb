780dff0d148ecf5439db2f41a36249d5e26bd29f
cori <heycorihey@icloud.com>
2021-02-01 04:30:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,28 +1,29 @@
+# Write your methods here
+
 def loop_message_five_times(message)
   count = 0
-while count < 5 do
-  puts message 
-  count += 1
+  while count < 5 do
+    puts message
+    count += 1
   end
 end
- 
+
 def loop_message_n_times(message, limit)
-   count = 0
-   while count < limit do
+  count = 0
+  while count < limit do
     puts message
-    count += 1 
+    count += 1
   end
 end
 
 def output_array(array)
   count = 0
-  while count < array.length
-do
+  while count < array.length do
     puts array[count]
     count += 1
   end
 end
-  
+
 def return_string_array(array)
   count = 0
   new_array = []
